97ba8726132684dd7adbb0a2d3e3d4dde21562ba
joelsalgado <joelsalgado1302@gmail.com>
2017-12-20 20:45:48 -0600

    api

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,6 +12,11 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'modules' =>[
+        'api' => [
+            'class' => 'frontend\modules\api\Api',
+        ]
+    ],
     'components' => [
         'view' => [
             'theme' => [
