787d051d62c6bc4cf2591df6f81ada5add7c67ba
joelsalgado <joelsalgado1302@gmail.com>
2018-01-29 13:11:15 -0600

    homeURL

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -20,13 +20,13 @@ use yii\helpers\Html;
 
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="/images/user.png" class="user-image" alt="User Image"/>
+                        <img src="<?= Yii::$app->homeUrl ?>images/user.png" class="user-image" alt="User Image"/>
                         <span class="hidden-xs"><?=  Yii::$app->user->identity->name ?></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
                         <li class="user-header">
-                            <img src="/images/user.png" class="img-circle"
+                            <img src="<?= Yii::$app->homeUrl ?>images/user.png" class="img-circle"
                                  alt="User Image"/>
                             <p>
                                 <?= Yii::$app->user->identity->name ?>
--- frontend/views/layouts/main-login.php
+++ frontend/views/layouts/main-login.php
@@ -18,7 +18,7 @@ dmstr\web\AdminLteAsset::register($this);
     <?php $this->head() ?>
     <link rel="shortcut icon" href="<?php echo Yii::$app->request->baseUrl; ?>/favicon2.ico" type="image/x-icon" />
 </head>
-<body background="/images/EDOMEX3.png">
+<body background="<?= Yii::$app->homeUrl ?>images/EDOMEX3.png">
 
 <?php $this->beginBody() ?>
 
